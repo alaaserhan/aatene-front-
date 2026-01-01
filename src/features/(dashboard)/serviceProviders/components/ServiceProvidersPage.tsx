303968062b3f66a7fdd8b35b73ea975f97b86981
Ahmed Mohamed <ahmedaltokey320@gmail.com>
2026-01-01 16:30:46 +0200

    .

--- src/features/(dashboard)/serviceProviders/components/ServiceProvidersPage.tsx
+++ src/features/(dashboard)/serviceProviders/components/ServiceProvidersPage.tsx
@@ -77,10 +77,10 @@ export function ServiceProvidersPage() {
     };
 
     const handleEditClick = (store: Store) => {
-        router.push(`/admin/stores/${store.id}/edit`);
+        router.push(`/admin/users?userId=${store.owner?.id}`);
     };
 
-     const handleShowClick = (store: Store) => {
+    const handleShowClick = (store: Store) => {
         router.push(`/admin/serviceProviders/${store.id}`);
     };
 
