2f9f7342da54b4a9b8daa463ec00eba1a96aea21
ghadaabdelhady11 <ghadaabdelhady11@gmail.com>
2026-05-09 17:12:17 +0300

    new

--- src/features/(web)/home/components/HomeSpecialServices.tsx
+++ src/features/(web)/home/components/HomeSpecialServices.tsx
@@ -23,12 +23,9 @@ export default function HomeSpecialServices({ services: initialServices }: HomeS
             
             <MaxWidthWrapper>
                 <div className="flex flex-col mb-10">
-                    <h2 className="text-xl md:text-2xl font-medium mb-2">
+                    <h2 className="text-xl md:text-2xl font-medium">
                         خدمات مميزة تم اختيارها لأجلك
                     </h2>
-                    {/* <p className="text-gray-2 text-sm md:text-base">
-                        أفضل الخدمات مبيعاً من بائعين موثوق بهم | ممول
-                    </p> */}
                 </div>
 
                 <div className="grid grid-cols-2 lg:grid-cols-4 gap-2 lg:gap-6">
