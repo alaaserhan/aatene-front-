e9a78a10f36218091b913505ed2e47e6bdf4443f
ghadaabdelhady11 <ghadaabdelhady11@gmail.com>
2026-05-11 23:30:20 +0300

    new

--- src/components/(web)/Navbar.tsx
+++ src/components/(web)/Navbar.tsx
@@ -61,7 +61,7 @@ const Navbar = () => {
             <NavbarCategoriesMenu variant="desktop" />
             <div className="min-w-0 flex-1 max-w-2xl">
               <Suspense fallback={<div className="h-10 w-full rounded-xl bg-gray-100/90 animate-pulse" aria-hidden />}>
-                <SearchBar currentLocale={lang} />
+                <SearchBar />
               </Suspense>
             </div>
           </div>
