eda9dc938b4bdf2a814449537d13bd5a33b293c3
Hossam Okasha <hossam.okasha.dev@gmail.com>
2026-06-19 11:50:46 +0300

    fix: swap navigation icons in HomeMultiBanners component

--- src/features/(web)/home/components/HomeMultiBanners.tsx
+++ src/features/(web)/home/components/HomeMultiBanners.tsx
@@ -62,13 +62,13 @@ export default function HomeMultiBanners({ banners }: HomeMultiBannersProps) {
                         onClick={() => scroll("right")}
                         className="absolute -left-4 top-1/2 -translate-y-1/2 z-20 bg-white shadow-lg rounded-full p-2 opacity-0 group-hover/section:opacity-100 transition-all hover:bg-gray-50 border border-gray-100"
                     >
-                        <ChevronRight className="w-6 h-6 text-black" />
+                    <ChevronLeft className="w-6 h-6 text-black" />
                     </button>
                     <button
                         onClick={() => scroll("left")}
                         className="absolute -right-4 top-1/2 -translate-y-1/2 z-20 bg-white shadow-lg rounded-full p-2 opacity-0 group-hover/section:opacity-100 transition-all hover:bg-gray-50 border border-gray-100"
                     >
-                        <ChevronLeft className="w-6 h-6 text-black" />
+                        <ChevronRight className="w-6 h-6 text-black" />
                     </button>
                 </div>
 
